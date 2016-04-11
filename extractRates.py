21dd407c11de53fa637669daf804c0f459210c3e
Dinyar Rabady <dinyar@rabady.at>
2016-04-11 13:09:02 +0200

    Small fixes.

--- extractRates.py
+++ extractRates.py
@@ -2,10 +2,13 @@
 import glob
 from datetime import datetime, timedelta
 import pytz
+import matplotlib as mpl
+mpl.use('Agg')
 from matplotlib import pyplot as plt
 from matplotlib.dates import date2num
 import argparse
 import time
+import sys
 
 desc = "Tool to extract rates from tdf dumps"
 
@@ -16,8 +19,7 @@ parser.add_argument('--outpath', type=str, default='', help='Folder to store plo
 parser.add_argument('--continuous', default=False, action='store_true', help='Whether to run in continuous mode.')
 opts = parser.parse_args()
 
-algos = ['|   41 |', '|   33 |', '|  165 |', '|  152 |']
-
+algos = ['|   41 |', '|   33 |', '|  165 |', '|  140 |', '|  154 |']
 
 if opts.path == '':
     path = ''
@@ -31,7 +33,8 @@ if opts.outpath == '':
 else:
     outpath = opts.outpath + '/'
 
-timeDelta = timedelta(hours=1)
+timeDelta = timedelta(minutes=10)
+#timeDelta = timedelta(minutes=25)
 while True:
     dateList = []
     rateList = [[] for x in xrange(0,len(algos))]
@@ -42,7 +45,8 @@ while True:
         day = int(datetimeString[6:8])
         hour = int(datetimeString[9:11])
         minute = int(datetimeString[11:13])
-        date = datetime(year, month, day, hour, minute)
+        second = int(datetimeString[13:15])
+        date = datetime(year, month, day, hour, minute, second)
         if (datetime.utcnow()-date) > timeDelta:
             continue
         dateList.append(date)
@@ -51,23 +55,26 @@ while True:
             for line in f:
                 for algo in algos:
                     if algo in line:
+                        #print index, algo, line
                         startPos = line.find(algo)+len(algo)
                         rate = int(line[startPos:line.find('|', startPos)])
+                        #print rate
                         rateList[index].append(rate)
                         index += 1
+    #print dateList, rateList
     
     x = [date2num(date) for date in dateList]
 
     fig = plt.figure()
     graph = fig.add_subplot(111)
     try:
-        graph.plot(x, rateList[1], 'ro', x, rateList[0], 'bs', x, rateList[2], 'y*', x, rateList[3], 'g^')
+        graph.plot(x, rateList[0], 'ro', x, rateList[1], 'bs', x, rateList[2], 'y*', x, rateList[3], 'mp', x, rateList[4], 'g^')
         graph.set_xticks(x)
-        
-        legend = graph.legend(['SingleEG5', 'SingleMuOpen', 'SingleJet8', 'BPTX AND'], numpoints=1)
+
+        legend = graph.legend(['SingleEG5', 'SingleMuOpen', 'SingleJet16', 'SingleJet36', 'BPTX AND'], numpoints=1, loc='upper left')
         
         graph.set_xticklabels(
-            [date.strftime("%Y-%m-%d-%H:%M") for date in dateList]
+            [date.strftime("%H:%M:%S") for date in dateList]
             )
         fig.autofmt_xdate()
         plt.ylabel('Rate [Hz]')
@@ -75,12 +82,17 @@ while True:
     
         plt.yscale('log', nonposy='clip')
         plt.savefig(outpath + 'uGTrates_log.png', bbox_inches='tight')
-    except ValueError:
+    except ValueError as e:
+        print "Value error: {0}".format(e)
         print "[ERROR]: Plotting failed."
-        print x, rateList[1], x, rateList[0], x, rateList[2], x, rateList[3]
+        print 'x', x, 'eg5', rateList[0], len(x), len(rateList[0])
+        print 'x', x, 'muopen', rateList[1], len(x), len(rateList[1])
+        print 'x', x, 'jet16', rateList[2], len(x), len(rateList[2])
+        print 'x', x, 'jet36', rateList[3], len(x), len(rateList[3])
+        print 'x', x, 'bptx', rateList[4], len(x), len(rateList[4])
     
     if opts.continuous is True:
-        time.sleep(60*5)
+        time.sleep(15*1)
     else:
         break
 
--- rateMonitor.py
+++ rateMonitor.py
@@ -48,45 +48,5 @@ while True:
     filename = 'rates_' + datetime.utcnow().strftime("%Y%m%d-%H%M%S")
     f = open(filename, 'w')
     subprocess.call(['tdf', 'run', 'rate_counters_menu_v4', 'gt_mp7.1'], stdout=f)
-    time.sleep(5*60)
-
-
-from datetime import datetime, timedelta
-import subprocess
-import time
-import zipfile
-import glob
-import os
-
-zipFilename = 'rateLogs_' + datetime.utcnow().strftime("%Y%m%d-%H%M%S")
-zf = zipfile.ZipFile('%s.zip' % (zipFilename), "w", zipfile.ZIP_DEFLATED)
-filenameStart = 'rates_'
-timeDelta = timedelta(hours=1)
-for filename in glob.glob(filenameStart + '*'):
-    datetimeString = filename[filename.find(filenameStart)+len(filenameStart):]
-    year = int(datetimeString[0:4])
-    month = int(datetimeString[4:6])
-    day = int(datetimeString[6:8])
-    hour = int(datetimeString[9:11])
-    minute = int(datetimeString[11:13])
-    date = datetime(year, month, day, hour, minute)
-
-    if (datetime.utcnow()-date) > timeDelta:
-        zf.write(filename)
-zf.close()
-# Removing those files separately to be protected from unexpected crashes.
-for filename in glob.glob(filenameStart + '*'):
-    datetimeString = filename[filename.find(filenameStart)+len(filenameStart):]
-    year = int(datetimeString[0:4])
-    month = int(datetimeString[4:6])
-    day = int(datetimeString[6:8])
-    hour = int(datetimeString[9:11])
-    minute = int(datetimeString[11:13])
-    date = datetime(year, month, day, hour, minute)
-
-    if (datetime.utcnow()-date) > timeDelta:
-        os.remove(filename)
-
-time.sleep(5)
-
+    time.sleep(1*60)
 
